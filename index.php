8998402694bd7c4aa19b26d92fcb4fc1049dca06
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-12-01 09:24:12 +0100

    Mini PHP framework initial setup

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
 
+use App\App;
+use App\Controllers\HomeController;
+
 require_once 'vendor/autoload.php';
 
-echo 'Hello';
+$app = new App;
+
+$container = $app->getContainer();
+
+$container['errorHandler'] = function () {
+    return function ($response) {
+
+        return $response->setBody('page not found')->withStatus(404);
+    };
+};
+
+$container['config'] = function () {
+    return [
+        'db_driver' => 'mysql',
+        'db_host' => 'event-planner.com',
+        'db_name' => 'event-planner',
+        'db_user' => 'root',
+        'db_password' => 'root'
+    ];
+};
+
+$container['db'] = function ($c) {
+    return new PDO(
+        $c->config['db_driver'] . ':host=' . $c->config['db_host'] . ';dbname=' . $c->config['db_name'],
+        $c->config['db_user'],
+        $c->config['db_password']
+    );
+};
+
+$app->get('/', [HomeController::class, 'index']);
+
+$app->run();
