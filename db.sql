7b89f9091da5060f106b634a683dd3d566067197
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-12-13 08:51:20 +0100

    Sql db creation code added

---
+++ db.sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5deb2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 13, 2021 at 08:48 AM
+-- Server version: 8.0.27-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `event-planner`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `location` varchar(255) NOT NULL,
+  `max_attendees` int NOT NULL,
+  `description` text NOT NULL,
+  `count` int NOT NULL DEFAULT '0',
+  `admin_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `name`, `date`, `location`, `max_attendees`, `description`, `count`, `admin_id`) VALUES
+(1, 'event', '2021-12-01 12:02:06', 'vukovarska', 40, 'dobar event', 0, 1),
+(2, 'event2', '2021-12-01 12:02:06', 'vukovarska2', 40, 'dobar event2', 0, 1),
+(3, 'test eventjgjhbjhaaaaaaaaaaa', '2021-12-13 18:27:00', 'centar', 40, 'Ovo je test event', 0, 2),
+(4, 'Sandro fds', '2021-12-09 14:10:00', 'centar', 32, 'jgfjgfjgf', 0, 2),
+(5, 'novi', '2021-12-18 14:37:00', 'dadsasd', 1, 'asffsafas', 0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `user_id` int UNSIGNED NOT NULL,
+  `event_id` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `surname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `surname`, `email`, `password`, `admin`) VALUES
+(1, 'sandro', 'blavicki', 'sandro@test.com', 'password', 1),
+(2, 'Sandro', 'Blavicki', 'sandro.blavicki@gmail.com', '$2y$12$tMI9eqDvrvD/LtWp7ZWP1OFjmocbVRgXqnD573/oB3pNDN/lvCuEa', 1),
+(3, 'Sandro', 'Blavicki', 'test@test.com', '$2y$12$VslaDfDKqedweQo7xYmWoOfM.5/.eU7nUezrrNSmOtysm2FJjNThC', 0),
+(4, 'Admin', 'Admin', 'admin@admin.com', '$2y$12$CBg56aVRJDsbMSAi25L.C.SkfFhcCjK3ds5Nbs0xB.Pn0ZLPVABmS', 0),
+(5, 'Test', 'Test', 'test2@test.com', '$2y$12$.aE.D6R27BCQsAc93TcH2un30Cl3fYp6of.hiIYq.ngQrxXyUZXJe', 0),
+(6, 'Dsffsdafasdafs', 'Fasddfasasd', 'test3@test.com', '$2y$12$XzqUHR.TDW.INWNmTJjSveJMnXvdblpRUerruM.txvoJgktYmYrle', 0),
+(7, 'Test', 'Test', 'test4@test.com', '$2y$12$JaZTr4HkNrJjtJmUwSKV5.XOXhds2Gl/UUlsQ4PAM3F5yG3WL36Fe', 0),
+(8, 'Fsdafsafasd', 'Fdfsadfasd', 'tefst52@test.com', '$2y$12$wE1Uu3E1TejP4KzbBgnv2.GQ6qt3jpB//HuIVZEI9kAitlunuXFNS', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`user_id`,`event_id`),
+  ADD KEY `fk_event_id` (`event_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `fk_event_id` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
