65be07f202f6850bfc080e622097698e61bc742e
Sandro Blavicki <sandro.blavicki@gmail.com>
2021-12-23 12:12:25 +0100

    Added page title

--- app/Components/Header.php
+++ app/Components/Header.php
@@ -15,6 +15,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
             integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
             crossorigin="anonymous"></script>
+    <title>Event Planner</title>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 </head>
 <body class="d-flex flex-column min-vh-100">
